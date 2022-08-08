d71c92c28d44e6f60a5a805d5eadf3aa122b1643
eb_summer <18189240263@163.com>
2022-08-08 18:08:39 +0800

    将原有的类别从包改为module

---
+++ leetcode/src/main/MakeLargestSpecial.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * @author summer
+ * @create 2022-08-08 16:00
+ * @Description 761. 特殊的二进制序列
+ *              https://leetcode.cn/problems/special-binary-string/
+ */
+public class MakeLargestSpecial {
+}
